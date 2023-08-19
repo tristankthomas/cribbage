01af6d3d0e1aac3f30a472710152a1f79834d89a
tristankthomas <tristankthomas@icloud.com>
2023-08-19 22:09:53 +1000

    Implemented rank conversion

--- cribbage.pl
+++ cribbage.pl
@@ -4,3 +4,22 @@
  * Date: 19-08-2023
  */
 
+% convert rank to numeric value for sorting
+rank_to_int(card(ace, _), _, 1).
+
+rank_to_int(card(jack, _), Type, Value) :-
+    ( Type == nosum ->
+        Value = 11 ;
+        Value = 10 ).
+rank_to_int(card(queen, _), Type, Value) :-
+    ( Type == nosum ->
+        Value = 12 ;
+        Value = 10 ).
+rank_to_int(card(king, _), Type, Value) :-
+    ( Type == nosum ->
+        Value = 13 ;
+        Value = 10 ).
+
+rank_to_int(card(Rank, _), _, Value) :-
+    integer(Rank),
+    Value is Rank.

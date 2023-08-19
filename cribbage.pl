93b90062a7bc41751628d1434056cd1133e67e5b
tristankthomas <tristankthomas@icloud.com>
2023-08-19 22:57:10 +1000

    Sorts cards by rank

--- cribbage.pl
+++ cribbage.pl
@@ -23,3 +23,21 @@ rank_to_int(card(king, _), Type, Value) :-
 rank_to_int(card(Rank, _), _, Value) :-
     integer(Rank),
     Value is Rank.
+
+% generate list of sortable cards by rank
+generate_list(Cards, Type, Res) :-
+    generate_list(Cards, Type, [], Res).
+
+generate_list([], _, Acc, Acc).
+
+generate_list([C|Cs], Type, Acc, Res) :-
+    rank_to_int(C, Type, Value),
+    append([Value], Acc, NewAcc),
+    generate_list(Cs, Type, NewAcc, Res).
+
+% hand value predicate
+hand_value(Hand, Startcard, Value) :-
+    % pairs and runs scoring
+    All = [Startcard|Hand],
+    generate_list(All, nosum, List),
+    msort(List, Sorted).
